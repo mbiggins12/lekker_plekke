4986c8a021b5e71d7ff6502c7cb8c5d613fb61a0
mbiggins12 <mbiggins@nd.edu>
2015-05-27 00:57:09 +0200

    lekker plekke app

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+place_cape_town = [
+  { name: "Table Mountain", description: "A plateau formed over thousands of years of erosion, which overlooks the whole city while offering gorgeous 360s of the ocean, city, and surrounding mountains." },
+  { name: "WoolsWorth", description: "The premier shopping establishment in all of Cape Town and quite possibly the entirety of South Africa." },
+  { name: "Waterfront Market", description: "An open market in Cape Town near the beach that is notable because of its absence of having been visited by me." },
+]
+
+places = Place.create(place_cape_town)
