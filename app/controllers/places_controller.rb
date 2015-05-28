7e7129a86711c2c211e3e8335aae064c472eb9e9
mbiggins12 <mbiggins@nd.edu>
2015-05-28 21:08:30 +0200

    Lekker Plekke updated with comments

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -10,8 +10,11 @@ class PlacesController < ApplicationController
   def create
   	@place = Place.new(place_params)
 
-  	@place.save
-  	redirect_to @places
+  	if @place.save
+	  	redirect_to @places
+	else
+		render :new
+	end
   end
   
   def new
@@ -25,15 +28,24 @@ class PlacesController < ApplicationController
   def update
   	@place = Place.find(params[:id])
 
-  	@place.update
-  	redirect_to @places
+  	if @place.update(place_params)
+	  	redirect_to @places
+	 else
+	 	render :edit
+	 end
   end
   
   def destroy
   	@place = Place.find(params[:id])
 
   	@place.destroy
-  	redirect_to @places
+	redirect_to places_path
+  end
+
+  private
+
+  def place_params
+  	params.require(:place).permit(:name, :description, :address)
   end
 
 end
--- app/models/place.rb
+++ app/models/place.rb
@@ -1,2 +1,4 @@
 class Place < ActiveRecord::Base
+	validates :name, presence: true
+	has_many :comments
 end
